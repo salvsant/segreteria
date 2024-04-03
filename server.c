07b3d38a3df194007ebc34b611550fb716e4aa7d
salvasant <salvatore.santaniello001@studenti.uniparthenope.it>
2024-04-03 12:15:43 +0200

    completata funzione addexamsession

--- server.c
+++ server.c
@@ -95,8 +95,61 @@ int main() {
         // Leggi la data dell'appello dalla socket
         read(connfd, date, sizeof(date));
 
+        // Query parametrizzata per l'inserimento di un appello
+        const char *query = "INSERT INTO exam_sessions (exam_session_name, session_date) VALUES (?, STR_TO_DATE(?, '%Y-%m-%d'))";
+
+// Creazione dello statement preparato
+        MYSQL_STMT *stmt = mysql_stmt_init(conn);
+        if (mysql_stmt_prepare(stmt, query, strlen(query)) != 0) {
+            const char *err = mysql_error(conn);
+            write(connfd, err, strlen(err) + 1);
+            mysql_stmt_close(stmt);
+            mysql_close(conn);
+            return;
+        }
+
+// Associazione dei parametri
+        MYSQL_BIND params[2];
+        memset(params, 0, sizeof(params));
+        params[0].buffer_type = MYSQL_TYPE_STRING;
+        params[0].buffer = name;
+        params[0].buffer_length = strlen(name);
+        params[1].buffer_type = MYSQL_TYPE_STRING;
+        params[1].buffer = date;
+        params[1].buffer_length = strlen(date);
+
+        if (mysql_stmt_bind_param(stmt, params) != 0) {
+            const char *err = mysql_error(conn);
+            write(connfd, err, strlen(err) + 1);
+            mysql_stmt_close(stmt);
+            mysql_close(conn);
+            return;
+        }
+
+// Esecuzione dello statement
+        if (mysql_stmt_execute(stmt) != 0) {
+            if (strstr(mysql_error(conn), "foreign key constraint fails")) {
+                const char *err = "non esiste un esame con questo nome!";
+                write(connfd, err, strlen(err) + 1);
+            } else {
+                const char *err = mysql_error(conn);
+                write(connfd, err, strlen(err) + 1);
+            }
+            mysql_stmt_close(stmt);
+            mysql_close(conn);
+            return;
+        }
+        else{// Chiusura dello statement
+            const char *ins = "inserimento del nuovo appello completato con successo!";
+            write(connfd, ins, strlen(ins) + 1);
+            mysql_stmt_close(stmt);
+            mysql_close(conn);}
+
+
 
     }
+
+
 
 
     void addExam{
