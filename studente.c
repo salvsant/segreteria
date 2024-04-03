8755b1dc6cf553c1c9442242bb61672fe7283339
santofemiano <santo.femiano001@studenti.uniparthenope.it>
2024-04-03 21:53:12 +0200

    connessione server lato studente.c

--- studente.c
+++ studente.c
@@ -17,4 +17,28 @@ int main (int argc, char **argv) {
         fprintf(stderr, "Utilizzo: %s <indirizzoIP>\n", argv[0]);
         exit(1);
     }
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Errore nella creazione della socket!");
+        exit(1);
+    }
+
+
+    servaddr.sin_family = AF_INET;
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+        fprintf(stderr, "Errore inet_pton per %s\n", argv[1]);
+        exit(1);
+    }
+    servaddr.sin_port = htons(1026);
+
+
+     // Connessione al server.
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        printf("Connessione fallita!\n");
+        exit(1);
+    }
+    else {
+        printf("Connessione stabilita!\n");
+    }
+
 }
