e125c1e35d543ee42885d0d6fa6d8766ea66a958
santofemiano <santo.femiano001@studenti.uniparthenope.it>
2024-04-03 16:58:09 +0200

    Introduco controlli e instauro connessioni di segreteria.c a server.c

--- segreteria.c
+++ segreteria.c
@@ -16,8 +16,46 @@ typedef struct {
 int main(int argc, char **argv) {
     int sockfd, listenfd;
     int behaviour;
+    int dim = 0;
+    int test = 0;
     int logical = 0;
+    char name[255] = {0};
+    char date[12] = {0};
+    char result[255] = {0};
+    char corso[255];
     struct sockaddr_in servaddr, secaddr;
     MYSQL *conn;
     Client_stud client_sockets[4096];
+
+
+    if (argc != 2) {
+        fprintf(stderr, "Utilizzo: %s <indirizzoIP>\n", argv[0]);
+        exit(1);
+    }
+
+
+    if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+        perror("Errore nella creazione della socket!");
+        exit(1);
+    }
+
+
+    servaddr.sin_family = AF_INET;
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+        fprintf(stderr, "Errore inet_pton per %s\n", argv[1]);
+        exit(1);
+    }
+    servaddr.sin_port = htons(1025);
+
+
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("Errore nell'operazione di connect!");
+        exit(1);
+    }
+
+
+
+
 }
+
+
