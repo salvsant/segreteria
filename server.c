0642b5191f3cba03f8e1f9d74c5c326d0dc48ca7
santofemiano <santo.femiano001@studenti.uniparthenope.it>
2024-04-03 11:34:05 +0200

    aggiungo librerie lato server

--- server.c
+++ server.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <mysql/mysql.h>
+#include <pthread.h>
 
 int main() {
     printf("Hello, World!\n");
