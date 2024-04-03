c3571cc777245f6e7d688c2391018cd9e3ee2ad2
salvasant <salvatore.santaniello001@studenti.uniparthenope.it>
2024-04-03 11:43:14 +0200

    definizioni variabili

--- server.c
+++ server.c
@@ -7,7 +7,18 @@
 #include <mysql/mysql.h>
 #include <pthread.h>
 
+typedef struct {
+    int connfd;
+} Client_stud;
+
 int main() {
+    int sockfd, listenfd;
+    int dim = 0;
+    struct sockaddr_in servaddr, secaddr;
+    MYSQL *conn;
+    Client_stud client_sockets[4096];
+
+
     printf("Hello, World!\n");
     printf("test commit");
     return 0;
