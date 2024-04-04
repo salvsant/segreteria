97d6628040880583cfd698e67996e532db0d306c
salvasant <salvatore.santaniello001@studenti.uniparthenope.it>
2024-04-04 19:22:06 +0200

    completato addbooking

--- server.c
+++ server.c
@@ -210,6 +210,135 @@ int main() {
 
     void addBooking(int connfd) {
 
+        MYSQL *conn = connection();
+
+        char exam_name[255] , exam_date[255];
+        // Leggi l'id dell'appello dalla socket
+        read(connfd, exam_name, sizeof(exam_name));
+        // Leggi la matricola dello studente dalla socket
+        read(connfd, exam_date, sizeof(exam_date));
+
+        // Query parametrizzata per l'inserimento di una prenotazione
+        const char *query = "INSERT INTO booking (exam_name, date, progressive_number)\n"
+                            "VALUES (?, STR_TO_DATE(?, '%Y-%m-%d'), (SELECT IFNULL(MAX(progressive_number), 0) + 1 FROM (SELECT * FROM booking) AS temp WHERE exam_name = ? AND date = STR_TO_DATE(?, '%Y-%m-%d')));";
+
+
+        // Creazione dello statement preparato
+        MYSQL_STMT *stmt = mysql_stmt_init(conn);
+        if (mysql_stmt_prepare(stmt, query, strlen(query)) != 0) {
+            write(connfd, mysql_error(conn), strlen(mysql_error(conn)) + 1);
+            perror("errore creazione statement");
+            mysql_stmt_close(stmt);
+            mysql_close(conn);
+            return;
+        }
+
+        // Associazione dei parametri
+        MYSQL_BIND params[4];
+        memset(params, 0, sizeof(params));
+        params[0].buffer_type = MYSQL_TYPE_STRING;
+        params[0].buffer = exam_name;
+        params[0].buffer_length = strlen(exam_name);
+        params[1].buffer_type = MYSQL_TYPE_STRING;
+        params[1].buffer = exam_date;
+        params[1].buffer_length = strlen(exam_date);
+        params[2].buffer_type = MYSQL_TYPE_STRING;
+        params[2].buffer = exam_name;
+        params[2].buffer_length = strlen(exam_name);
+        params[3].buffer_type = MYSQL_TYPE_STRING;
+        params[3].buffer = exam_date;
+        params[3].buffer_length = strlen(exam_date);
+
+        if (mysql_stmt_bind_param(stmt, params) != 0) {
+            write(connfd, mysql_error(conn), strlen(mysql_error(conn)) + 1);
+            perror("errore biding");
+            mysql_stmt_close(stmt);
+            mysql_close(conn);
+            return;
+        }
+
+        // Esecuzione dello statement
+        if (mysql_stmt_execute(stmt) != 0) {
+            write(connfd, mysql_error(conn), strlen(mysql_error(conn)) + 1);
+            perror("errore esecuzione");
+            mysql_stmt_close(stmt);
+            mysql_close(conn);
+            return;
+        }
+        perror("esecuzione success");
+
+        // Chiusura dello statement
+        mysql_stmt_close(stmt);
+
+        const char *ins = "Inserimento della nuova prenotazione completato con successo!";
+        write(connfd, ins, strlen(ins) + 1);
+
+        const char *max_query = "SELECT MAX(progressive_number) AS max_progressive_number FROM booking WHERE exam_name = ? AND date = ?";
+
+        MYSQL_STMT *max_stmt = mysql_stmt_init(conn);
+        if (mysql_stmt_prepare(max_stmt, max_query, strlen(max_query)) != 0) {
+            write(connfd, mysql_error(conn), strlen(mysql_error(conn)) + 1);
+            perror("errore creazione statement");
+            mysql_stmt_close(max_stmt);
+            mysql_close(conn);
+            return;
+        }
+
+        MYSQL_BIND max_params[2];
+        memset(max_params, 0, sizeof(max_params));
+        max_params[0].buffer_type = MYSQL_TYPE_STRING;
+        max_params[0].buffer = exam_name;
+        max_params[0].buffer_length = strlen(exam_name);
+        max_params[1].buffer_type = MYSQL_TYPE_STRING;
+        max_params[1].buffer = exam_date;
+        max_params[1].buffer_length = strlen(exam_date);
+
+        if (mysql_stmt_bind_param(max_stmt, max_params) != 0) {
+            write(connfd, mysql_error(conn), strlen(mysql_error(conn)) + 1);
+            perror("errore biding");
+            mysql_stmt_close(max_stmt);
+            mysql_close(conn);
+            return;
+        }
+
+        if (mysql_stmt_execute(max_stmt) != 0) {
+            write(connfd, mysql_error(conn), strlen(mysql_error(conn)) + 1);
+            perror("errore esecuzione");
+            mysql_stmt_close(max_stmt);
+            mysql_close(conn);
+            return;
+        }
+
+        MYSQL_BIND max_result;
+        memset(&max_result, 0, sizeof(max_result));
+        int max_progressive_number;
+        max_result.buffer_type = MYSQL_TYPE_LONG;
+        max_result.buffer = &max_progressive_number;
+
+        if (mysql_stmt_bind_result(max_stmt, &max_result) != 0) {
+            write(connfd, mysql_error(conn), strlen(mysql_error(conn)) + 1);
+            perror("errore biding risultato");
+            mysql_stmt_close(max_stmt);
+            mysql_close(conn);
+            return;
+        }
+
+// Fetch del risultato
+        if (mysql_stmt_fetch(max_stmt) != 0) {
+            write(connfd, mysql_error(conn), strlen(mysql_error(conn)) + 1);
+            perror("errore fetch risultato");
+            mysql_stmt_close(max_stmt);
+            mysql_close(conn);
+            return;
+        }
+        mysql_stmt_close(max_stmt);
+
+
+        write(connfd, &max_progressive_number, sizeof (max_progressive_number));
+
+
+        mysql_close(conn);
+
 
 
 
